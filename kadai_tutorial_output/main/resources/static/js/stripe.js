26a57b5c122f3e3ba2d11eb2fe049c518afdee21
tuyokairu <yuri492@yahoo.co.jp>
2024-09-22 01:40:36 +0900

    Delete kadai_tutorial_output/main directory

--- kadai_tutorial_output/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51PyyUJ034wxKpBOisOLRhlOd4wTYqBexHAkk0bJN5GcPRKPd7LfrNJRy2q8R6hdNvrsk4zEPxytwoFLKGts1iIfl00awtMIY3N');
- const paymentButton = document.querySelector('#paymentButton');
- 
- paymentButton.addEventListener('click', () => {
-   stripe.redirectToCheckout({
-     sessionId: sessionId
-   })
- });
